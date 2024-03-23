b95b3169a82c14aaf466bec7760e74c3c9158a5d
DS0708 <tlsehdtjd070@naver.com>
2024-03-23 23:36:53 +0900

    Chapter03 - 신경망 - 3.2 활성화함수

---
+++ Ch03/sigmoid.py
@@ -0,0 +1,13 @@
+import numpy as np
+import matplotlib.pylab as plt
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+x = np.array([-1.0, 1.0, 2.0])
+print(sigmoid(x))
+
+x_graph = np.arange(-5.0, 5.0, 0.1)
+y = sigmoid(x_graph)
+plt.plot(x_graph,y)
+plt.ylim(-0.1, 1.1) # y축 범위 지정
+plt.show()
