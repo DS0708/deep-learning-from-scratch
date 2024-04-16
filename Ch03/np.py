e1210973ca145f9ee2fdc84e544d8ee7e635bf3e
DS0708 <tlsehdtjd070@naver.com>
2024-04-16 14:08:47 +0900

    Chapter05 Backpropagation - 5.6.1

--- Ch03/np.py
+++ Ch03/np.py
@@ -11,6 +11,22 @@ print(np.dot(A,D))
 X = np.array([1,2])
 W = np.array([[1,3,5],[2,4,6]])
 Y = np.dot(X,W)
+
+X2 = np.array([[1],[2]])
+X3 = np.array([1,2])
+
+print("-----------------")
+print(X.shape)
+print(X2.shape)
+print(X3.shape)
+print(W.shape)
+XX2 = np.dot(X,X2)
+XX3 = np.dot(X,X3)
+print(XX2.shape)
+print(XX3.shape)
+print(XX3)
+print("-----------------")
+
 print(Y)
 
 Q = np.array([[1,2],[1,2],[0,1]])
@@ -20,4 +36,6 @@ Z = np.array([1,1])
 print(Z.shape)
 
 print(np.dot(Q,Z))
+
+
 
